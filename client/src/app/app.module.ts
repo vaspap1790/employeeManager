0540a3fabd4e29e03ed76fe3415ee7dcb922d63b
vaspap1790 <vaspap1790@gmail.com>
2021-09-18 00:09:35 +0300

    add and edit through UI

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { EmployeeService } from './employee.service';
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, FormsModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
